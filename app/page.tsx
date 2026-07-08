364d8e4de8c37f598bb84203d724834d8ccfc2b0
JOHNSON1MILLION <168733525+JOHNSON1MILLION@users.noreply.github.com>
2026-07-08 16:11:48 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,40 +4,97 @@ import Image from "next/image";
 import {CSSProperties, useState} from 'react'
 import './globals.css'
 
-function MyButton() {
-  return (
-    <button>
-      HELLO SIR
-    </button>
-  );
+interface RewardSegment {
+  id : number,
+  color : string,
+  percentage : number,
+  rotation : number,
 }
-
 export default function MyApp() {
+
+  const [segments, setSegments] = useState<RewardSegment[]>([
+    {id : 0, color : 'blue', percentage : 50, rotation : 0},
+  ])
+
+  const addRandomSegment = () => {
+    let segment_percentage = 100
+    if (segments.length != 0) {
+      segment_percentage = 100/(segments.length+1)
+    }
+    addSegment(randomHexColor(), segment_percentage, 0)
+    console.log(segment_percentage)
+  }
+
+  const addSegment = (color : string, percentage : number, rotation : number) => {
+    const newSegment = {
+      id : Date.now(),
+      color : color,
+      percentage : percentage,
+      rotation : rotation,
+    }
+    setSegments([...segments, newSegment])
+  }
+
+  const deleteSegment = (id : number) => {
+    setSegments(segments.filter(segment => segment.id !== id))
+  }
+
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-      <Reward_Segment />
+      <button onClick = {addRandomSegment}>ADD RANDOM SEGMENT</button>
+      <Reward_Wheel segments = {segments} />
     </div>
   );
 }
-
-
-
-function Reward_Segment(){
-  
-  const [color, set_color] = useState('purple');
-  const [percentage, set_percentage] = useState(99);
-
-
+interface RewardSegmentProps {
+  segments : RewardSegment[],
+}
+function Reward_Wheel({segments} : RewardSegmentProps){
   return (
-   <div style = {Reward_Segment_style(color, percentage)}>
+    /*<Reward_Segment 
+      _color = 'red'
+      _percentage = {60}
+    />*/
 
+    <div>
+      {
+        segments.map((segment) => (
+          <Reward_Segment
+            key = {segment.id}
+            // TODO: how to get rid of this duplicate key and id?
+            id = {segment.id}
+            color = {segment.color}
+            //_color = 'red'
+            //_percentage = segment.percentage,
+            percentage = {segment.percentage}
+            rotation = {segment.rotation}
+          />
+        ))
+      }
     </div>
   )
 }
 
-const Reward_Segment_style = (color : string, percentage : number): CSSProperties => {
+function Reward_Segment({color, percentage, rotation} : RewardSegment){
+  
+  //const [_color, set_color] = useState(_color);
+  //const [_percentage, set_percentage] = useState(_percentage);
+  //const [rotation, set_rotation] = useState(0);
+
+  //const handleMouseMove = () => {
+    //console.log("MOUSE MOVED")
+    //set_rotation(rotation+1)
+  //}
+
+  return (
+    <div
+      //onMouseMove = {handleMouseMove}
+      style = {Reward_Segment_style(color, percentage, rotation)}>
+    </div>
+  )
+}
+
+const Reward_Segment_style = (color : string, percentage : number, rotation : number): CSSProperties => {
   
   // calcuate the 
   let clip_path_string : string = "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)"
@@ -53,10 +110,10 @@ const Reward_Segment_style = (color : string, percentage : number): CSSPropertie
 
     clip_path_string += x_string + "% " + y_string + "%"
 
-    console.log(x_value)
-    console.log(y_value)
-    console.log(x_string)
-    console.log(y_string)
+    //console.log(x_value)
+    //console.log(y_value)
+    //console.log(x_string)
+    //console.log(y_string)
 
     if (percentage > 87) {
       clip_path_string += ", 100% 0%"
@@ -73,8 +130,6 @@ const Reward_Segment_style = (color : string, percentage : number): CSSPropertie
     clip_path_string += ")"
   }
 
-  console.log(clip_path_string)
-
   return {
     position : 'absolute',
     width : '80%',
@@ -82,70 +137,19 @@ const Reward_Segment_style = (color : string, percentage : number): CSSPropertie
     borderRadius : "50%",
     backgroundColor : color,
     clipPath : clip_path_string,
+    rotate : String(rotation) + 'deg',
   }
 }
 
-/*
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
-}*/
+
+
+
+// misc functions
+const randomHexColor = (): string => {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+};
+
+//const randomNumberRange(min : number, max : number): number {
+//}
